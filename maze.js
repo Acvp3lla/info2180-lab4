ab046cf897ceae73888395af9e786f04883b2734
Acvp3lla <lucianogordon@hotmail.com>
2017-10-18 20:12:55 -0500

    All Boundary Turns Red.

--- maze.js
+++ maze.js
@@ -1,9 +1,36 @@
+/*
 
-
-//Task 1
+Task 1
 
 window.onload = function (){
 	document.getElementById('boundary1').onmouseover = function(){
 		document.getElementById('boundary1').classList.add("youlose");
 	}
 }
+*/
+
+//Declaration of Variables
+startMaze = false; //Set to false because the Maze has not been entered.
+endMaze = false; //Set to false because the Maze has not been completed.
+touchedNoBound = true; //Intitial State of the Boundary
+
+window.onload = function(){
+
+
+	start = document.getElementById("start"); //Start of Maze
+	end   = document.getElementById("end"); //End of Maze
+	boundaries = document.querySelectorAll(".boundary"); //Boundaries of Maze
+	maze = document.getElementById("maze");	//Body of the Maze
+
+	//Boundarys changes to red if the mouse cursor hovers over it.
+	for (i=0;i<boundaries.length;i++){
+		boundaries[i].addEventListener("mouseover", function(element){
+			
+		document.getElementById("status").innerHTML = "YOU LOSE";
+
+			for (i=0;i<boundaries.length;i++){
+				boundaries[i].classList.add("youlose")
+			}
+			touchedNoBound = false;
+		});
+	};
