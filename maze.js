b843945e84c71535d200728c855eb189aa2e452d
Acvp3lla <lucianogordon@hotmail.com>
2017-10-18 19:57:00 -0500

    Single Boundary Turns Red.

---
+++ maze.js
@@ -0,0 +1,9 @@
+
+
+//Task 1
+
+window.onload = function (){
+	document.getElementById('boundary1').onmouseover = function(){
+		document.getElementById('boundary1').classList.add("youlose");
+	}
+}
